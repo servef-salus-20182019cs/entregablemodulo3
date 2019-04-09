e6d5fef11d8daf5098d56156be3801bfa75ccc38
charly <49226878+carlosoleroig@users.noreply.github.com>
2019-04-09 11:00:32 +0200

    Update modelo.js

--- modelo/modelo.js
+++ modelo/modelo.js
@@ -6,5 +6,22 @@ const historialJugador = (historial, callback)=>{
 		return callback(error, resultado);
 	})
 }
+const insertaJugador = (datos, callback)=>{
+	/*console.log("entro aquí 2");*/
+	conexion.query('INSERT INTO jugadores (nombre, equipo_actual, fecha_fichaje) VALUES ("' + datos.nombre + '", "' + datos.equipo + '", "' + datos.anyo +'");', (error,resultado)=>{
+		if (error) throw error;
+		else {
+			/*console.log("entro aquí 4")*/
+            return callback(error,resultado);
+        }
+        
+    });
+};
+const team =(datos,callback)=>{
+	conexion.query('SELECT e.club, j.nombre FROM jugadores AS j INNER JOIN jugador_equipo AS jE ON (j.id_jugador = jE.id_jugador) INNER JOIN equipos AS e ON (e.id_equipo = jE.id_equipo) WHERE e.club LIKE "%"?"%" fecha_fin between "20190101" and "20190702"' ), (error,resultado)=>{
 
-module.exports = {historialJugador};
+	}
+}
+
+
+module.exports = {historialJugador,team,};
